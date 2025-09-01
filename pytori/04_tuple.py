330df146042e9b26d62c0a23181a84511fa22fa6
keigo03456-beep <keigo03456@gmail.com>
2025-09-02 00:12:07 +0900

    Add 04_tuple.py

--- pytori/04_tuple.py
+++ pytori/04_tuple.py
@@ -1,6 +1,11 @@
-tuple1 = "a","b"
-print(tuple1)
-tuple2 = "c","d","e"
-print(tuple2)
-tuple3 = tuple1 + tuple2
-print(tuple3)
+#ランダムに曜日と月と数字を選ぶ
+import random
+
+tuple_days = ("Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun")
+tuple_months = ("Jan", "Feb", "Mar", "Apr", "May", "Jun", 
+                "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
+tuple_pi = (3.14, 1.59, 2.65, 3.58)
+
+print(random.choice(tuple_days))    
+print(random.choice(tuple_months))  
+print(random.choice(tuple_pi))      
