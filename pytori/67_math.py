8c41e8e699091119903d1ce863d24bb4740361f3
keigo03456-beep <keigo03456@gmail.com>
2025-09-07 20:58:15 +0900

    67_math

---
+++ pytori/67_math.py
@@ -0,0 +1,11 @@
+import math
+
+print(math.pi)      
+print(math.e)      
+
+print(math.sqrt(16)) 
+print(math.factorial(5)) 
+
+print(math.sin(math.pi/2)) 
+print(math.cos(0))          
+print(math.tan(math.pi/4))
